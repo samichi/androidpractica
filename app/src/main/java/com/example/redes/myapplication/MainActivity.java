50cc8514c0893162ec7b3492e77ad94bce63200c
samichi <leonbohorquezisabel@gmail.com>
2018-11-07 06:36:19 -0500

    Creando Actividad

--- app/src/main/java/com/example/redes/myapplication/MainActivity.java
+++ app/src/main/java/com/example/redes/myapplication/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.redes.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "TestActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate... ");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause...");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume... ");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop... ");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy... ");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart... ");
     }
 }
